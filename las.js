73019bb40ff55f515e860bda5d4164c209d65990
Soyo Solution <xenialaw418@gmail.com>
2015-03-25 11:50:09 +0800

    Update las.js for v1.1
    - Add a scroll bar if too many record displayed at suggestion menu.
    - Add and renew 21 json data with programming language name and description for demo purpose.
    - Better appearance, add style padding on search box and suggestion menu.
    - Add js value sugItemH and disNum for configuration.
    - Changed default json key name as "key", description key name to "value".

--- las.js
+++ las.js
@@ -1,16 +1,17 @@
 /*!
-* Light-jQuery-Auto-Suggestion v1.0
+* Light-jQuery-Auto-Suggestion v1.1
 * https://github.com/soyosolution/Light-jQuery-Auto-Suggestion
 * http://tool.soyosolution.com/light-jquery-auto-suggestion/
 *
 * Includes jQuery Library
 * http://www.jquery.com/
 *
-* Copyright 2015 Soyo Solution Company. and other contributors
+* Copyright 2015 Soyo Solution. and other contributors
 * Released under the MIT license
 * http://jquery.org/license
 *
-* Date: 2015-03-09
+* Start Date       : 2015-03-09
+* Last Updated Date: 2015-03-25
 */
 
 //Make config
@@ -18,9 +19,11 @@ var sugDataUrl             = "suggestlist.json";     //URL to get Json suggestLi
 var searchId               = "las_search_panel";     //Search Panel Wrapper Id Name which including search box.
 var searchBoxId            = "las_textbox";          //Search Input Name Id name
 var sugMenuId              = "las_suggestbox";       //Suggest Menu Id name
-var sugMenuW               = "600px";                //Width of Search input box and Suggest menu, 
-var jsonKey1               = "key_text";             //json Frame Key 1
-var jsonKey2               = "desc_text";            //json Frame Key 2
+var sugMenuW               = "550px";                //Width of Search input box and Suggest menu, 
+var sugItemH               = "25";                   //Line-Height of suggested items
+var dispNum                = "6";                    //Display suggested item Number, for example, value is 10 means display 10 items
+var jsonKey1               = "key";                  //json Frame Key 1
+var jsonKey2               = "value";                //json Frame Key 2
 
 
 $( document ).ready(function() {
@@ -28,8 +31,8 @@ $( document ).ready(function() {
     var soyoObject = sacGetJsonData();
     //Embuded css style
     jQuery('#'+searchId).css("position","relative");
-    jQuery('#'+searchId).append('<ul id="'+sugMenuId+'" style="position:absolute; width:'+sugMenuW+'; top:'+jQuery('#'+searchBoxId).outerHeight()+'px; left:0px; list-style-type:none; background:#fff; border:1px solid #ccc; margin:0;padding:0;z-index:10; display:none;"></ul>');   
-    jQuery('#'+searchBoxId).css("width",sugMenuW);
+    jQuery('#'+searchId).append('<ul id="'+sugMenuId+'" style="position:absolute; width:'+sugMenuW+';top:'+jQuery('#'+searchBoxId).outerHeight()+'px; left:0px; list-style-type:none; background:#fff; border:1px solid #ccc; margin:0;padding-left:10px;z-index:10; display:none; max-height:'+sugItemH*dispNum+'px; overflow:auto;"></ul>');   
+    jQuery('#'+searchBoxId).css({"width":sugMenuW, "padding-left":"10px"});
     
     //============== Hide suggestion if click outside searchbox =============
     $(document).click(function(event) { 
@@ -64,11 +67,14 @@ function searchAndSuggest(obj, keyword) {
     var itemCounter = 0;
     var tempStr = "";
     var objSize = obj.length;
+    keyword = keyword.toLowerCase();    
     for (var i=0; i<objSize;i++) {
-        if (obj[i][jsonKey1].search(keyword) != -1) {
+        var jsonVal = obj[i][jsonKey1].toLowerCase();
+        var temp = jsonVal.search(keyword);
+        if ( temp != -1) {
             resultArr.push(obj[itemCounter][jsonKey1]);
             var tempParams = '\''+obj[i][jsonKey1]+'\'';
-            tempStr += '<li onclick="selectsug('+tempParams+')">'+obj[i][jsonKey1]+'    <span style="color:#aaa">'+obj[i][jsonKey2]+"</span></li>";
+            tempStr += '<li onclick="selectsug('+tempParams+')" style="line-height:'+sugItemH+'px;">'+obj[i][jsonKey1]+'    <span style="color:#aaa;padding-left:20px;">'+obj[i][jsonKey2]+"</span></li>";
             itemCounter++;
             jQuery('#'+sugMenuId).css("display","block");
         }
